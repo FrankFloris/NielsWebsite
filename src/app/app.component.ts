dbd356d7aa44c8eb3f5c0628fef8d8d4d8d81c28
FrankFloris <frank_floris@hotmail.com>
2019-10-17 18:26:14 +0200

    knop en bestelformulier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,64 +17,24 @@ export class AppComponent {
     title: Title
   ) {}
 
-  removeActiveClass() {
+  private removeActiveClass() {
     const header = document.getElementById('nav');
     const link = header.getElementsByClassName('active');
-    // tslint:disable-next-line:prefer-for-of
     for (let i = 0; i < link.length; i++) {
       link[i].className = link[i].className.replace(' active', '');
     }
-
   }
 
-  naarAboutMe() {
-    this.removeActiveClass();
-    const deze = document.getElementById('about-me');
+  private setActiveClass(destination: string) {
+    const deze = document.getElementById(destination);
     deze.className += ' active';
-    this.router.navigate(['/about-me']);
   }
 
-  naarHome() {
+  navigateTo(destination: string) {
     this.removeActiveClass();
-    const deze = document.getElementById('home');
-    deze.className += ' active';
-    this.router.navigate(['/home']);
+    this.setActiveClass(destination);
+    this.router.navigate(['/' + destination]);
   }
 
-  naarFractalshop() {
-    this.removeActiveClass();
-    const deze = document.getElementById('fractalshop');
-    deze.className += ' active';
-    this.router.navigate(['/fractalshop']);
-  }
 
-  naarAboutfractals() {
-    this.removeActiveClass();
-    const deze = document.getElementById('aboutfractals');
-    deze.className += ' active';
-    this.router.navigate(['/aboutfractals']);
-  }
-
-  // naarFractalshopOld() {
-  //   this.removeActiveClass();
-  //   const deze = document.getElementById('fractalshop');
-  //   deze.className += ' active';
-  //   this.router.navigate(['/fractalshop']);
-  //   this.setToFalse();
-  //   this.setOldToTrue();
-  // }
-
-  // setToFalse() {
-  //   FractalshopComponent.prototype.old = false;
-  //   FractalshopComponent.prototype.new = false;
-  //   FractalshopComponent.prototype.all = false;
-  //   FractalshopComponent.prototype.structured = false;
-  //   FractalshopComponent.prototype.unstructured = false;
-  //   FractalshopComponent.prototype.micro = false;
-  // }
-  //
-  // setOldToTrue() {
-  //   this.setToFalse();
-  //   FractalshopComponent.prototype.old = true;
-  // }
 }
